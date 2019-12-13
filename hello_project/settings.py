7762fe271b37e3ebe97261583df35dc6bcd9943f
SMTG <serignemortallagueye@gmail.com>
2019-12-13 21:26:35 +0000

    users app commit

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig', # new
+    'users.apps.UsersConfig', # new
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.CustomUser' # new
