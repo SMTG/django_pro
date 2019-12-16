5e4c34d6ed13cb43c0186fb4bca459a0d450932e
SMTG <serignemortallagueye@gmail.com>
2019-12-16 20:44:36 +0000

    fin ch12

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
